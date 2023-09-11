026fc6ecc5582a05edc8d2d7f31d2920510b739e
박일훈 <chiwoo2074@gmail.com>
2023-09-12 01:41:59 +0900

    application port

---
+++ flitter/src/main/kotlin/dev/hoon/deepdive/heavytraffic/flitter/application/port/exception/TimelineException.kt
@@ -0,0 +1,6 @@
+package dev.hoon.deepdive.heavytraffic.flitter.application.port.exception
+
+/**
+ * 타임라인 조회 실패 예외
+ */
+class CannotReadTimelineException(): RuntimeException()
--- flitter/src/main/kotlin/dev/hoon/deepdive/heavytraffic/flitter/application/port/in/WritePostUseCase.kt
+++ flitter/src/main/kotlin/dev/hoon/deepdive/heavytraffic/flitter/application/port/in/WritePostUseCase.kt
@@ -1,9 +1,18 @@
 package dev.hoon.deepdive.heavytraffic.flitter.application.port.`in`
 
 import dev.hoon.deepdive.heavytraffic.flitter.application.port.dto.PostDto
+import dev.hoon.deepdive.heavytraffic.flitter.application.port.exception.CannotWritePostException
 
+/**
+ * 포스트 작성 유스 케이스
+ */
 interface WritePostUseCase {
-
+    /**
+     * 작성
+     *
+     * @param postDto 작성 내용
+     * @throws CannotWritePostException 포스트 작성 실패 예외
+     */
+    @Throws(CannotWritePostException::class)
     fun write(postDto: PostDto.Request)
-
 }
