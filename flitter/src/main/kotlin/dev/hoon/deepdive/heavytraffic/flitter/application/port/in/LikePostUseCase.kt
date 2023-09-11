16c62e3f768fc1ee692d22da33643f4d082bf462
박일훈 <chiwoo2074@gmail.com>
2023-09-12 01:41:59 +0900

    application port

--- flitter/src/main/kotlin/dev/hoon/deepdive/heavytraffic/flitter/application/port/in/LikePostUseCase.kt
+++ flitter/src/main/kotlin/dev/hoon/deepdive/heavytraffic/flitter/application/port/in/LikePostUseCase.kt
@@ -1,9 +1,28 @@
 package dev.hoon.deepdive.heavytraffic.flitter.application.port.`in`
 
+import dev.hoon.deepdive.heavytraffic.flitter.application.port.exception.CannotLikePostException
+import dev.hoon.deepdive.heavytraffic.flitter.application.port.exception.CannotUnLikePostException
+
+/**
+ * 포스트 좋아요 유스 케이스
+ */
 interface LikePostUseCase {
-
+    /**
+     * 좋아요
+     *
+     * @param memberId 회원 아이디
+     * @param postId 좋아요할 포스트 아이디
+     * @throws CannotLikePostException 좋아요 실패 시
+     */
+    @Throws(CannotLikePostException::class)
     fun like(memberId: Long, postId: Long)
-
+    /**
+     * 좋아요 취소
+     *
+     * @param memberId 회원 아이디
+     * @param postId 좋아요 취소할 포스트 아이디
+     * @throws CannotUnLikePostException 좋아요 취소 실패 시
+     */
+    @Throws(CannotUnLikePostException::class)
     fun unLike(memberId: Long, postId: Long)
-
 }
