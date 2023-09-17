5feb299c1424850569fe63288fe87396c08ad393
박일훈 <chiwoo2074@gmail.com>
2023-09-18 00:34:14 +0900

    id 타입 변경 (String -> UUID)

--- flitter/src/main/kotlin/dev/hoon/deepdive/heavytraffic/flitter/application/port/dto/TimelineDto.kt
+++ flitter/src/main/kotlin/dev/hoon/deepdive/heavytraffic/flitter/application/port/dto/TimelineDto.kt
@@ -4,12 +4,13 @@ import dev.hoon.deepdive.heavytraffic.flitter.domain.member.Member
 import dev.hoon.deepdive.heavytraffic.flitter.domain.post.Post
 import dev.hoon.deepdive.heavytraffic.flitter.domain.timeline.Timeline
 import java.time.LocalDateTime
+import java.util.UUID
 
 class TimelineDto {
     data class Response(
-        val id: Long,
-        val postId: Long,
-        val writerId: Long,
+        val id: UUID,
+        val postId: UUID,
+        val writerId: UUID,
         val writerNickname: String,
         var contents: String,
         var like: Long = 0L,
