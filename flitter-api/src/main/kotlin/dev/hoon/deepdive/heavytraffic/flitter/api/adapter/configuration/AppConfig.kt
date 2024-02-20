1cf52c479f03f2400cf239d27a0345c2ac28a9d6
박일훈 <chiwoo2074@gmail.com>
2024-02-20 14:05:18 +0900

    [feature/5] 설정 분리
    1. rabbitMQ와 objectMapper 설정 분리

---
+++ flitter-api/src/main/kotlin/dev/hoon/deepdive/heavytraffic/flitter/api/adapter/configuration/AppConfig.kt
@@ -0,0 +1,44 @@
+package dev.hoon.deepdive.heavytraffic.flitter.api.adapter.configuration
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+@Configuration
+class AppConfig {
+
+    @Bean
+    fun objectMapper() {
+        val objectMapper = ObjectMapper()
+        val javaTimeModule = JavaTimeModule()
+
+        javaTimeModule.addSerializer(LocalDateTime::class.java, CustomLocalDateTimeSerializer())
+        javaTimeModule.addDeserializer(LocalDateTime::class.java, CustomLocalDateTimeDeSerializer())
+
+        objectMapper.registerKotlinModule().registerModule(JavaTimeModule())
+    }
+
+    class CustomLocalDateTimeSerializer(): JsonSerializer<LocalDateTime>() {
+        override fun serialize(value: LocalDateTime, gen: JsonGenerator, serializers: SerializerProvider) {
+            gen.writeString(formatter.format(value))
+        }
+    }
+
+    class CustomLocalDateTimeDeSerializer(): JsonDeserializer<LocalDateTime>(){
+        override fun deserialize(p: JsonParser, ctxt: DeserializationContext): LocalDateTime {
+            return LocalDateTime.parse(p.text, formatter)
+        }
+    }
+
+    companion object {
+        private const val dateTimeFormat = "yyyy-MM-dd HH:mm:ss"
+        private val formatter = DateTimeFormatter.ofPattern(dateTimeFormat, Locale.KOREA)
+    }
+}
